6111cdca7047623f1c802273169f84cfa3627627
patricksandquist <patricksandquist@gmail.com>
2016-01-18 17:48:37 -0800

    Add game and board logic

--- board.rb
+++ board.rb
@@ -1,10 +1,71 @@
 require_relative 'tile.rb'
 
 class Board
-  def initialize
-
+  def initialize(grid_size, num_bombs)
+    @grid_size = grid_size
+    make_grid!
+    plant_bombs!(num_bombs)
   end
 
   def in_range?(pos)
+    pos.all? { |coord| (0...@grid_size).include?(coord) }
+  end
+
+  def won?
+    @grid.flatten.all? do |tile|
+      !tile.bombed? == tile.explored?
+    end
+  end
+
+  def lost?
+    @grid.flatten.any? { |tile| tile.bombed? && tile.explored? }
+  end
+
+  def over?
+    won? || lost?
+  end
+
+  def [](pos)
+    # Syntactic sugar for accessing tiles
+    row, col = pos
+    @grid[row][col]
+  end
+
+  def to_s
+    # Override the default to_s method
+    output = ""
+
+    @grid.each do |row|
+      row.each do |tile|
+        output += tile.render
+      end
+      output += "\n"
+    end
+
+    output
+  end
+
+  private
+
+  def make_grid!
+    # Make a new 2D square array for our minefield
+    @grid = Array.new(@grid_size) do |i|
+      Array.new(@grid_size) do |j|
+        Tile.new(self, [i, j])
+      end
+    end
+  end
+
+  def plant_bombs!(num_bombs)
+    until num_bombs.zero?
+      pos = [rand(@grid_size), rand(@grid_size)]
+      tile = self[pos]
+
+      # Skip if there is already a bomb there
+      next if tile.bombed?
+
+      num_bombs -= 1
+      tile.plant_bomb
+    end
   end
 end
--- game.rb
+++ game.rb
@@ -1,7 +1,56 @@
 require_relative 'board.rb'
 
 class Game
-  def initialize
+  LAYOUTS = {
+    :small => { :grid_size => 9, :num_bombs => 10 },
+    :medium => { :grid_size => 16, :num_bombs => 40 },
+    :large => { :grid_size => 32, :num_bombs => 160 } # whoa.
+  }
 
+  def initialize(size)
+    params = LAYOUTS[size]
+    @board = Board.new(params[:grid_size], params[:num_bombs])
+    play_game
   end
+
+  private
+
+  def play_game
+    until @board.over?
+      puts @board
+      move = get_move
+      make_move(move)
+    end
+
+    puts @board
+
+    if @board.won?
+      puts "You won!"
+    else
+      puts "You lost!"
+    end
+  end
+
+  def get_move
+    puts "Do you want to [f]lag or [e]xplore a tile?"
+    action = gets.chomp.downcase[0]
+
+    puts "What position on the grid?"
+    pos = gets.chomp.split(",").map { |e| e.to_i }
+
+    {:action => action, :pos => pos}
+  end
+
+  def make_move(move)
+    case move[:action]
+    when "f"
+      @board[move[:pos]].toggle_flag
+    when "e"
+      @board[move[:pos]].reveal
+    end
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  MinesweeperGame.new(:small).play
 end
